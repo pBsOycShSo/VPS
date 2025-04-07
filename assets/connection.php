3c6da6c0530ce803ffc1d75766c32cf21be9b1fe
JK <82444721+pBsOycShSo@users.noreply.github.com>
2025-04-07 15:30:50 +0530

    Update connection.php

--- assets/connection.php
+++ assets/connection.php
@@ -2,7 +2,7 @@
 $user = $_POST["user"];
 $password = $_POST["pas"];
 $connect = new mysqli("localhost","root","LakshanSQL","vps", "3306");
-$q = "SELECT * FROM user WHERE `username` = '".$user."' AND `password` = '".$password."'";
+$q = "SELECT * FROM user WHERE `username` = '".$user."' AND `password` = '".$password."' AND `status` = 1";
 $r = $connect->query($q);
 if ($r->num_rows == 1) {
     echo "Done";
